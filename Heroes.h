82d4f267e63c5f14b304d0211b130d1be1ff18d9
Megan De Leon <megandeleon@weber.edu>
2026-05-04 13:40:08 -0600

    Add Heroes.h header file with character definitions

---
+++ Heroes.h
@@ -0,0 +1,18 @@
+#ifndef HEROES_H
+#define HEROES_H
+
+#include "Personaje.h"
+#include <string>
+
+// --- SECCIÓN DE PERSONALIZACIÓN ---
+// TODO: Elige nombres épicos para tus héroes
+const std::string MI_GUERRERO = "NombreAqui";
+const std::string MI_MAGO = "NombreAqui";
+const std::string MI_ARQUERO = "NombreAqui";
+// ----------------------------------
+
+class Guerrero : public Personaje {
+    // ... tu implementación
+};
+// ... resto de clases
+#endif
