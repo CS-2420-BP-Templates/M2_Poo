2892076267f5008434502fc2e168e6a3e7c20587
Megan De Leon <megandeleon@weber.edu>
2026-05-04 12:45:29 -0600

    Initialize party vector and add TODOs for implementation
    Added vector to hold party of characters and TODOs for implementation.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <vector>
+#include ""
 
+int main() {
+    std::vector<Personaje*> party;
+    // TODO: Instanciar héroes con 'new'
+    // TODO: Ejecutar bucle de ataque polimórfico
+    // TODO: Liberar memoria con 'delete'
+    return 0;
+}
