fa80bb92b4f501d32f470b98326736597e5db150
Megan De Leon <megandeleon@weber.edu>
2026-05-04 14:06:51 -0600

    Add protected attributes to Personaje class
    Added protected attributes 'nombre' and 'salud' to the Personaje class and updated comments for clarity.

--- Personaje.h
+++ Personaje.h
@@ -6,13 +6,22 @@
 
 class Personaje {
 protected:
-    //TODO agregar los dos con estatus protegidos 
-    //
+    // TODO: Agregar los atributos 'nombre' (string) y 'salud' (int)
+    // Recuerda que 'protected' permite que las clases hijas los vean.
+
 public:
+    // Constructor
     Personaje(std::string n, int s);
+
+    // Destructor Virtual: Crucial para la limpieza de memoria en polimorfismo
     virtual ~Personaje(); 
-    virtual void ataque() = 0; // Función virtual pura
-    void recibirGolpe(int cantidad);
+
+    // TODO: Una función atacar Virtual Pura (= 0), lo que hace a la clase Abstracta.
+    
+    // Método común para todas las clases derivadas
+    void recibirDanio(int cantidad);
+
+    std::string getNombre() const { return nombre; }
 };
 
 #endif
